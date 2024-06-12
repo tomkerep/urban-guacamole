7bec6ed4f55b141fd92f9f10f244324fb22517a0
lenabrender <lena.brender@hs-weingarten.de>
2024-06-12 15:00:27 +0200

    Poppins Font hinzugefügt + global.css erstellt
    Poppins Font hinzugefügt + global.css erstellt

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,18 @@
+
+
+
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+
+
+
+	theme: { 
+		extend: {
+		fontFamily: {
+		  poppins: ['Poppins', 'sans-serif'],
+		},
+	  },
 	
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],	
