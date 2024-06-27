8a5bd48b71a51256f40c55d3a6f079d39352488e
lenabrender <lena.brender@hs-weingarten.de>
2024-06-27 16:18:51 +0200

    Contact und Indexseite
    Startseite bearbeitet und Kontaktseite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],	
 	daisyui: {
-		themes: ["bumblebee",
+		themes: ["nord",
 			/*{
 			  light: {
 				...require("daisyui/src/theming/themes")["light"],
