0e164c56d2482162cd5bc5ab2e7818fe9d59872e
tomkerep <tom.kerep@rwu.de>
2024-06-27 16:31:36 +0200

    back to update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,28 @@
+
+
+
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+<<<<<<< HEAD
 	theme: {
 		
+=======
+
+
+
+	theme: { 
+		extend: {
+		fontFamily: {
+		  poppins: ['Poppins', 'sans-serif'],
+		},
+	  },
+>>>>>>> Contact_page
 	
 	},
 	plugins: [require("@tailwindcss/typography"), require("daisyui")],	
 	daisyui: {
-		themes: ["bumblebee",
+		themes: ["nord",
 			/*{
 			  light: {
 				...require("daisyui/src/theming/themes")["light"],
